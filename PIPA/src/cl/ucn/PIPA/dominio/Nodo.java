bc6c1f58df208334804449cda4d704e37a575fec
Giacomo "Giacum" Baldessari <103346031+gbaldessari@users.noreply.github.com>
2023-09-05 13:05:33 -0300

    Clase 4

--- PIPA/src/cl/ucn/PIPA/dominio/Nodo.java
+++ PIPA/src/cl/ucn/PIPA/dominio/Nodo.java
@@ -1,5 +1,32 @@
 package cl.ucn.PIPA.dominio;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Nodo {
-    
+    private int id;
+    private int posX;
+    private int posY;
+    private List<Arista> aristas;
+
+    public Nodo(int id,int posX,int posY){
+        this.id = id;
+        this.posX = posX;
+        this.posY = posY;
+        aristas = new ArrayList<>();
+    }
+    public int getId(){
+        return id;
+    }
+    public int getPosX(){
+        return posX;
+    }
+    public int getPosY(){
+        return posY;
+    }
+    public void addArista(Arista arista){
+        aristas.add(arista);
+    }
+    public Arista getArista(int pos){
+        return aristas.get(pos);
+    }
 }
